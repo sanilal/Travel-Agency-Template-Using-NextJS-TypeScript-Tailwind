241801c8d82c0b35a47c09c476de6d2bd5f12ebf
Sanilal P Sathyan <sanilal@gmail.com>
2024-05-14 18:03:18 +0400

    Header components added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Topbar from "./components/Topbar";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Topbar />
+        <Header />
         {children}
         </body>
     </html>
