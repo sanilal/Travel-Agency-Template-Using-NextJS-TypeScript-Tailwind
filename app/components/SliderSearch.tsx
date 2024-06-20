02e2fddd47145c32ba4c4b8083e42e84e3020e5d
Sanilal P Sathyan <sanilal@gmail.com>
2024-06-20 17:59:54 +0400

    slider

--- app/components/SliderSearch.tsx
+++ app/components/SliderSearch.tsx
@@ -1,13 +1,14 @@
+"use client"
 import { useState } from "react"
 
 export default function SliderSearch() {
     const [openTab, setOpenTab] = useState(1)
   return (
     <>
-      <div className="flex flex-wrap">
-        <div className="w-full">
+      <div className="flex flex-wrap -top-[60px] relative">
+        <div className="w-full flex items-center justify-center flex-col">
           <ul
-            className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+            className="flex mb-0 list-none flex-wrap pt-3 flex-row max-w-[600px] "
             role="tablist"
           >
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
@@ -26,7 +27,7 @@ export default function SliderSearch() {
                 href="#link1"
                 role="tablist"
               >
-                <i className="fas fa-space-shuttle text-base mr-1"></i> Profile
+                <i className="fas fa-space-shuttle text-base mr-1"></i> Packages
               </a>
             </li>
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
@@ -45,7 +46,7 @@ export default function SliderSearch() {
                 href="#link2"
                 role="tablist"
               >
-                <i className="fas fa-cog text-base mr-1"></i>  Settings
+                <i className="fas fa-cog text-base mr-1"></i>  Air Tickets
               </a>
             </li>
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
@@ -64,11 +65,30 @@ export default function SliderSearch() {
                 href="#link3"
                 role="tablist"
               >
-                <i className="fas fa-briefcase text-base mr-1"></i>  Options
+                <i className="fas fa-briefcase text-base mr-1"></i>  Visa
+              </a>
+            </li>
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                  (openTab === 4
+                    ? "text-white bg-blueGray-600"
+                    : "text-blueGray-600 bg-white")
+                }
+                onClick={e => {
+                  e.preventDefault();
+                  setOpenTab(4);
+                }}
+                data-toggle="tab"
+                href="#link4"
+                role="tablist"
+              >
+                <i className="fas fa-briefcase text-base mr-1"></i>  Activities
               </a>
             </li>
           </ul>
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+          <div className="relative flex flex-col min-w-0 break-words bg-th-pink w-full mb-6 shadow-lg rounded">
             <div className="px-4 py-5 flex-auto">
               <div className="tab-content tab-space">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
@@ -93,6 +113,16 @@ export default function SliderSearch() {
                   </p>
                 </div>
                 <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+                  <p>
+                    Efficiently unleash cross-media information without
+                    cross-media value. Quickly maximize timely deliverables for
+                    real-time schemas.
+                    <br />
+                    <br /> Dramatically maintain clicks-and-mortar solutions
+                    without functional solutions.
+                  </p>
+                </div>
+                <div className={openTab === 4 ? "block" : "hidden"} id="link4">
                   <p>
                     Efficiently unleash cross-media information without
                     cross-media value. Quickly maximize timely deliverables for
